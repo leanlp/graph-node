69bc719ac20607b2b8ca091852d066ce2fc1d1bf
Leonardo Yvens <leoyvens@gmail.com>
2024-02-02 15:09:22 +0000

    fix rebase artifacts

--- runtime/wasm/src/host_exports.rs
+++ runtime/wasm/src/host_exports.rs
@@ -11,6 +11,7 @@ use never::Never;
 use semver::Version;
 use web3::types::H160;
 
+use graph::blockchain::BlockTime;
 use graph::blockchain::Blockchain;
 use graph::components::store::{EnsLookup, GetScope, LoadRelatedRequest};
 use graph::components::subgraph::{
@@ -25,6 +26,7 @@ use graph::prelude::serde_json;
 use graph::prelude::{slog::b, slog::record_static, *};
 use graph::runtime::gas::{self, complexity, Gas, GasCounter};
 pub use graph::runtime::{DeterministicHostError, HostExportError};
+use graph::schema::TypeKind;
 
 use crate::module::WasmInstance;
 use crate::{error::DeterminismLevel, module::IntoTrap};
@@ -352,7 +354,7 @@ impl HostExports {
         let entity_type = state.entity_cache.schema.entity_type(&entity_type)?;
         Self::expect_object_type(&entity_type, "remove")?;
 
-        let key = entity_type.parse_key_in(entity_id, self.data_source_causality_region)?;
+        let key = entity_type.parse_key_in(entity_id, self.data_source.causality_region)?;
         self.check_entity_type_access(&key.entity_type)?;
 
         gas.consume_host_fn_with_metrics(
@@ -376,7 +378,7 @@ impl HostExports {
         let entity_type = state.entity_cache.schema.entity_type(&entity_type)?;
         Self::expect_object_type(&entity_type, "get")?;
 
-        let store_key = entity_type.parse_key_in(entity_id, self.data_source_causality_region)?;
+        let store_key = entity_type.parse_key_in(entity_id, self.data_source.causality_region)?;
         self.check_entity_type_access(&store_key.entity_type)?;
 
         let result = state.entity_cache.get(&store_key, scope)?;
@@ -1114,7 +1116,7 @@ pub mod test_support {
     use std::{collections::HashMap, sync::Arc};
 
     use graph::{
-        blockchain::{BlockTime, Blockchain},
+        blockchain::BlockTime,
         components::{
             store::{BlockNumber, GetScope},
             subgraph::SharedProofOfIndexing,
