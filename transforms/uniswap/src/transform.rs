c6604ad304f42abb6459f30a63fecdfc4ddf8772
Filipe Azevedo <filipe@azevedo.io>
2024-02-20 09:56:56 +0000

    add some events

--- transforms/uniswap/src/transform.rs
+++ transforms/uniswap/src/transform.rs
@@ -8,8 +8,11 @@ use substreams_ethereum::pb::eth::v2::Block;
 use graph::indexer::{BlockTransform, EncodedBlock, EncodedTriggers, State};
 
 use crate::abi::factory::events::PoolCreated;
+use crate::abi::pool::events::{
+    Burn, Collect, CollectProtocol, Flash, Initialize, Mint, SetFeeProtocol, Swap,
+};
 
-const UNISWAP_V2_FACTORY: &str = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
+const UNISWAP_V3_FACTORY: &str = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
 pub const POOL_TAG: &str = "pool";
 
 #[derive(BorshSerialize, BorshDeserialize)]
@@ -25,9 +28,19 @@ pub struct Events {
     events: Vec<Event>,
 }
 
+type PoolAddress = Vec<u8>;
+
 #[derive(BorshSerialize, BorshDeserialize)]
 pub enum Event {
     PoolCreated(Pool),
+    Collect(PoolAddress),
+    CollectProtocol(PoolAddress),
+    Flash(PoolAddress),
+    Initialize(PoolAddress),
+    Mint(PoolAddress),
+    SetFeeProtocol(PoolAddress),
+    Swap(PoolAddress),
+    Burn(PoolAddress),
 }
 
 #[derive(Clone, Debug)]
@@ -39,7 +52,7 @@ impl UniswapTransform {
     #[allow(unused)]
     pub fn new() -> Self {
         Self {
-            factory_addr: Address::from_str(UNISWAP_V2_FACTORY).unwrap(),
+            factory_addr: Address::from_str(UNISWAP_V3_FACTORY).unwrap(),
         }
     }
 }
@@ -54,7 +67,32 @@ impl BlockTransform for UniswapTransform {
                 continue;
             }
 
-            if log.address() != self.factory_addr.as_ref() {
+            if Collect::match_log(&log.log) {
+                events.push(Event::Collect(log.address().to_vec()));
+            }
+            if CollectProtocol::match_log(&log.log) {
+                events.push(Event::CollectProtocol(log.address().to_vec()));
+            }
+            if Flash::match_log(&log.log) {
+                events.push(Event::Flash(log.address().to_vec()));
+            }
+            if Initialize::match_log(&log.log) {
+                events.push(Event::Initialize(log.address().to_vec()));
+            }
+            if Mint::match_log(&log.log) {
+                events.push(Event::Mint(log.address().to_vec()));
+            }
+            if SetFeeProtocol::match_log(&log.log) {
+                events.push(Event::SetFeeProtocol(log.address().to_vec()));
+            }
+            if Swap::match_log(&log.log) {
+                events.push(Event::Swap(log.address().to_vec()));
+            }
+            if Burn::match_log(&log.log) {
+                events.push(Event::Burn(log.address().to_vec()));
+            }
+
+            if log.address() == self.factory_addr.as_ref() {
                 continue;
             }
 
